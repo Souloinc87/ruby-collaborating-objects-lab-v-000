660e5ba3437958c2f652545db5c90ae52a29ffcd
Marcus  = Soulo <souloinc87@gmail.com>
2018-04-19 19:49:11 +0000

    automatically backed up by learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -46,7 +46,8 @@ class Artist
   end
 
   def self.find_or_create_by_name(name)
-    self.create_by_name(name) || self.find_by_name(name)
+    if self.find_by_name(name) == nil 
+    self.create_by_name(name) 
 
   end
 
