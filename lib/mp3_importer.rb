8f6ba38b2b324ee341663dd2a2596a7f478aea9c
Marcus  = Soulo <souloinc87@gmail.com>
2018-04-19 20:47:58 +0000

    automatically backed up by learn

--- lib/mp3_importer.rb
+++ lib/mp3_importer.rb
@@ -12,6 +12,6 @@ class MP3Importer
 
   def import
 
-  end 
+  end
 
 end
--- lib/song.rb
+++ lib/song.rb
@@ -5,4 +5,12 @@ class Song
     @name = name
   end
 
+  def self.new_by_filename
+    name_split = filename.split(" - ")
+    song = self.new(name_split[1])
+    artist = Artist.find_or_create_by_name(name_split[0])
+    song.artist = artist
+    artist.add_song(song)
+  end
+
 end
