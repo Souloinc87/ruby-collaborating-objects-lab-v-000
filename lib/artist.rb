90e2487e5093a5ab7c794fab34a014eb8f8eeb37
Marcus  = Soulo <souloinc87@gmail.com>
2018-04-19 19:33:28 +0000

    automatically backed up by learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -32,6 +32,14 @@ class Artist
     song
   end
 
+  def self.new_by_name(name)
+    song = self.new
+    song.name = name
+    song
+  end
+
+  end
+
   def self.find_by_name(name)
     @@all.detect{|x| x.name == name}
   end
