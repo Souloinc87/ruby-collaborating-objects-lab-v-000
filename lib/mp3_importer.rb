bc5a27eea02226c9bf3b92a7fe3c2df3b315682e
Marcus  = Soulo <souloinc87@gmail.com>
2018-04-19 20:56:09 +0000

    automatically backed up by learn

--- lib/mp3_importer.rb
+++ lib/mp3_importer.rb
@@ -11,7 +11,10 @@ class MP3Importer
   end
 
   def import
-
+    self.files.each do |file|
+      song = Song.new_by_filename(file)
+      Artist.all << song.artist unless Artist.all.include?(song.artist)
+    end 
   end
 
 end
