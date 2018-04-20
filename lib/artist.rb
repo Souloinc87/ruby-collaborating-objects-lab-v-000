fcfe190ff0a0f2078175b5a6b853d232a758ebf8
Marcus  = Soulo <souloinc87@gmail.com>
2018-04-20 01:50:02 +0000

    automatically backed up by learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -27,23 +27,6 @@ class Artist
     @@all << self
   end
 
-  def self.create_by_name(name)
-    artist = Artist.new(name)
-    artist.name = name
-    artist.save
-    artist
-  end
-
-  def self.new_by_name(name)
-    artist = Artist.new(name)
-    artist.name = name
-    artist
-  end
-
-  def self.find_by_name(name)
-    @@all.detect{|x| x.name == name}
-  end
-
   def self.find_or_create_by_name(name)
   self.all.detect {|x| x.name == name} || Artist.new(name)
   end
